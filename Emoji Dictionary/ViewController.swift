f5d63c25943e8b6c5ff68f3c89a32d3b56d4775a
Ege Akertek <ege.akertek@gmail.com>
2019-01-23 17:20:36 +0100

    Initial Commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Ege Akertek on 23.01.2019.
+//  Copyright © 2019 Ege Akertek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
